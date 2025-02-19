bd0241bacd76a26098c39c800dfa2f65c62e4d73
Ludovic Saule <ludovic_saule@hotmail.fr>
2025-02-19 15:33:27 +0100

    task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,6 +42,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :due_date)
+    params.require(:task).permit(:title, :description, :due_date, :completed)
   end
 end
---
+++ db/migrate/20250219141156_change_completed_default_in_tasks.rb
@@ -0,0 +1,6 @@
+class ChangeCompletedDefaultInTasks < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :tasks, :completed, false
+    change_column_null :tasks, :completed, false
+  end
+end
